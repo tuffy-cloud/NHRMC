ff7efbac5d2ef2532f35cb7d461a19929f92cc07
Kevin Tuffner <kevin.tuffner@thinkahead.com>
2018-12-13 08:34:32 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ provider "aws" {
 
 # Create the AMI Instance
 
-resource "aws_instance" "example" {
+resource "aws_instance" "web" {
   ami           = "ami-0ac019f4fcb7cb7e6"
   instance_type = "t1.micro"
 
